5ab8b529946dc212d2109e79d946d1aa2688353d
Sngho12 <163092385+Sngho12@users.noreply.github.com>
2025-02-19 14:34:42 +0900

    Update weather_script.py

--- weather_script.py
+++ weather_script.py
@@ -1,14 +1,10 @@
-# openweather api로 요청을 해서
-# 그 결과로 csv로 저장하는 py
-
-
 import requests
 import csv
 from datetime import datetime
 import os
 
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 CITY = "Seoul"
-API_KEY = "9fadb9e4f9dc7a4dd06af1292e205930"
 URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
 response = requests.get(URL)
@@ -17,5 +13,24 @@ print(data)
 temp = data["main"]["temp"]
 humidity = data["main"]["humidity"]
 description = data["weather"][0]["description"]
-print(temp, humidity, description)
-print(datetime.now())
+timezone = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(temp, humidity, description, timezone)
+
+# 위 4개의 데이터를 가지는 csv 파일 생성
+csv_filename = "seoul_weather.csv"
+header = ["timezone", "temp", "humidity", "description"]
+
+file_exist = os.path.isfile(csv_filename)
+
+# mode a -> 
+# if if not file, create
+# if is file, write
+with open(csv_filename, "a", newline="") as file:
+    writer = csv.writer(file)
+
+    if not file_exist:
+        writer.writerow(header)
+
+    writer.writerow([timezone, temp, humidity, description])
+
+    print("csv 저장 완료")
